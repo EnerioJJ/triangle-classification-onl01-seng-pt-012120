aa471ea77e94e7d26ae879239ee5f34e9592a166
Jay W <januarydwilson@gmail.com>
2020-03-15 18:16:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
+require_relative '../lib/triangle'
+
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+  
+  def kind
+    if x == y && y == z && z == x
+      return :equilateral
+    else if x == y && y == x but y != z
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
 end
